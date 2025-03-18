9e9c71759e5ff2fd1ece7e7f35808bbbdc7abdf1
Ayushman Mahana <666ayushman@gmail.com>
2025-03-18 10:20:51 +0530

    Some changes

--- frontend/projectIntelit/src/routes/AppRoutes.jsx
+++ frontend/projectIntelit/src/routes/AppRoutes.jsx
@@ -6,6 +6,8 @@ import About from '../screens/About.jsx'
 import Contact from '../screens/Contact.jsx'
 import BrowseCourses from '../screens/BrowseCourses.jsx'
 import Home from '../screens/Home.jsx'
+import TeacherLogin from '../screens/TeacherLogin.jsx'
+import TeacherRegister from '../screens/TeacherRegister.jsx'
 
 const AppRoutes = () => {
     return (
@@ -17,6 +19,8 @@ const AppRoutes = () => {
                 <Route path="/about" element={<About />} />
                 <Route path="/browsecourses" element={<BrowseCourses />} />
                 <Route path="/contact" element={<Contact />} />
+                <Route path='/teacherlogin' element={<TeacherLogin />} />
+                <Route path='/teacherregister' element={<TeacherRegister />} />
             </Routes>
         </BrowserRouter>
     )
